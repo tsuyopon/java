143f354686303c4f62c062ab269a71ee1f7488d4
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2022-07-13 10:02:44 +0900

    コメントの追加

--- Basic/HelloWorld/src/main/java/GraddleApp/App.java
+++ Basic/HelloWorld/src/main/java/GraddleApp/App.java
@@ -4,6 +4,10 @@
  */
 package GraddleApp;
 
+// extendsしないクラスはスーパークラスを持たないのではなく、javaライブラリで提供されている「java.lang.Object」クラスをスーパークラスに持ちます。
+// つまり、Objectクラスのメンバ(変数やメソッド)のいくつかが引き継がれていることを頭に入れておいてください。以下の資料を確認しておいてください。
+// 公式: https://docs.oracle.com/javase/jp/8/docs/api/java/lang/Object.html
+// 参考: https://code4u.jp/archives/604
 public class App {
     public String getGreeting() {
         return "こんにちは世界!";
